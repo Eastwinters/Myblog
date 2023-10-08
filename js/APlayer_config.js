1f029d55a257037ffc054d63a487b43ef9b1301c
Eastwinters <eastwinterliao@gmail.com>
2023-10-08 21:44:41 +0800

    Site updated: 2023-10-08 21:44:39

---
+++ js/APlayer_config.js
@@ -0,0 +1,23 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    theme: '#b7daff',
+    loop: 'all',
+    order: 'list',
+    preload: 'auto',
+    volume: 0.5,
+    //lrcType: 3,
+    mutex: true,
+    listFolded: false,
+
+    audio: [{
+            name: '每次相聚都是为了分离',
+            artist: '新裤子',
+            lrc: '/music/lrc/一直很安静 - 阿桑.lrc',
+            cover: 'http://pic.eastwinters.com/202309291253873.jpg',
+            url: 'http://music.163.com/song/media/outer/url?id=1883144306.mp3'
+                  },
+    
+        ]
+});
