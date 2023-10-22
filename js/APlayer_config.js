b4b18ccacb2f0167eaa3b7c251ddc8d4d19f9b07
Eastwinters <eastwinterliao@gmail.com>
2023-10-22 16:40:43 +0800

    Site updated: 2023-10-22 16:40:40

--- js/APlayer_config.js
+++ js/APlayer_config.js
@@ -10,8 +10,17 @@ const ap = new APlayer({
     //lrcType: 3,
     mutex: true,
     listFolded: false,
-
     audio: [{
+        name: '时の回廊',
+        artist: '光田康典',
+        lrc: '/music/lrc/一直很安静 - 阿桑.lrc',
+        cover: 'http://pic.eastwinters.com/202310221617575.jpg',
+        url: 'http://music.163.com/song/media/outer/url?id=468658.mp3'
+              },
+
+    ]
+
+     [{
             name: '每次相聚都是为了分离',
             artist: '新裤子',
             lrc: '/music/lrc/一直很安静 - 阿桑.lrc',
@@ -20,4 +29,5 @@ const ap = new APlayer({
                   },
     
         ]
+        
 });
